290f5cd4dc925b8530f0c4e1ed422b695d1c46eb
Caroline FCF <carolinefcosta.96@gmail.com>
2025-09-20 12:19:47 -0300

    Criação da array para armazenar os nomes.

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Array amigos
+let amigos = []
+
+// Boas vindas
+alert ("Bem-Vindo ao site Amigo secreto!")
+alert ("Adicione todos os nomes que deseja sortear e depois click em 'sortear amigo'.")
+
+// Função para adicionar amigo
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Digite um nome válido!");
+        return;
+    }
+
+    amigos.push(nome);
+    // teste saida da lista
+    console.log(`Lista: ${amigos}`);
+
+    input.value = "";
+}
